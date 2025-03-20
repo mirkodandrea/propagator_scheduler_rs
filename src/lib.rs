0121a93098c1dda9f5142897e8c37cedbfb1a4f4
mirkodandrea <mirko.dandrea@gmail.com>
2025-03-20 15:57:02 +0100

    fix missing dimension

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ const PRECISION: f64 = 10.0;
 /// Uses a **BinaryHeap** to maintain order efficiently.
 #[pyclass(module = "propagator_scheduler_rs", name = "Scheduler")]
 pub struct Scheduler {
-    heap: BinaryHeap<Reverse<(u32, Vec<[usize; 3]>)>>, // Min-heap of (time, updates)
+    heap: BinaryHeap<Reverse<(u32, Vec<Vec<[usize; 3]>>)>>, // Min-heap of (time, updates)
 }
 
 #[pymethods]
@@ -21,13 +21,13 @@ impl Scheduler {
     }
 
     /// Pushes an update into the scheduler in **O(log n) time**.
-    pub fn push(&mut self, coords: Vec<[usize; 3]>, time: f64) {
+    pub fn push(&mut self, coords: Vec<Vec<[usize; 3]>>, time: f64) {
         let time_key = f64::round(time * PRECISION) as u32;
         self.heap.push(Reverse((time_key, coords)));
     }
 
     /// Pushes multiple updates in **O(m log n) time** (m = # of updates).
-    pub fn push_all(&mut self, updates: Vec<(f64, Vec<[usize; 3]>)>) {
+    pub fn push_all(&mut self, updates: Vec<(f64, Vec<Vec<[usize; 3]>>)>) {
         for (time, coords) in updates {
             let time_key = f64::round(time * PRECISION) as u32;
             self.heap.push(Reverse((time_key, coords)));
@@ -35,7 +35,7 @@ impl Scheduler {
     }
 
     /// Pops the earliest event in **O(log n) time**.
-    pub fn pop(&mut self) -> Option<(f64, Vec<[usize; 3]>)> {
+    pub fn pop(&mut self) -> Option<(f64, Vec<Vec<[usize; 3]>>)> {
         self.heap
             .pop()
             .map(|Reverse(event)| (f64::from(event.0) / PRECISION, event.1))
@@ -45,8 +45,10 @@ impl Scheduler {
     pub fn active(&self) -> Vec<usize> {
         let mut unique_values: FxHashSet<usize> = FxHashSet::default();
         for Reverse((_time, coords_list)) in &self.heap {
-            for row in coords_list {
-                unique_values.insert(row[2]); // Extract the third element
+            for coords in coords_list {
+                for row in coords {
+                    unique_values.insert(row[2]);
+                }
             }
         }
         let mut result: Vec<usize> = unique_values.into_iter().collect();
